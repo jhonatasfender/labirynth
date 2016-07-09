d0829a3b8562fd17b41d0304abd6cd418122867e
root <root@debian.debian>
2016-07-09 14:10:31 -0300

    create project

---
+++ labyrinth.js
@@ -0,0 +1,11 @@
+var cols = 26, // number columns 
+        rows = 19, // number rows 
+        size = 30, // tamanho de cada tiles
+        tiles = []; // tiles matris
+function getAvailable(i, j) {
+    var available = [],
+            len = tiles[i][j].neighbours.length;
+    for (var ix = 0; ix < len; ix++) {
+        var t = tiles[i][j]
+    }
+}
